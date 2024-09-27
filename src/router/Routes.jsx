a7273378b79360b1a6e22b5277eb0b0c763026a8
Eider4 <167330642+Eider4@users.noreply.github.com>
2024-09-27 15:02:01 -0500

    route correction

--- src/router/Routes.jsx
+++ src/router/Routes.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "../pages/App";
 import CharacterDetail from "../pages/character-detail/CharacterDetail";
-import { MainPage } from "../pages/main/MainPage";
 import Favorites from "../pages/favorites/Favorites";
+import { MainPage } from "../pages/main-page/MainPage.jsx";
 
 export default function Routers() {
   return (
